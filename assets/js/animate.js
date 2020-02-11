aab3f0287902d2a41e1d5c7587dbd5ca821d9a5e
pattyprince <pattyprince@gmail.com>
2020-02-11 19:28:40 -0300

    Ajustes JS
    Animacion con falla.

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,6 +1,6 @@
 var $animation_elements = $('.animation-element');
 var $window = $(window);
-​
+
 function check_if_in_view() {
   var window_height = $window.height();
   var window_top_position = $window.scrollTop();
@@ -15,12 +15,12 @@ function check_if_in_view() {
     //check to see if this current container is within viewport
     if ((element_bottom_position >= window_top_position) &&
         (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
+      $element.addClass('inview');
     } else {
-      $element.removeClass('in-view');
+      $element.removeClass('inview');
     }
   });
 }
-​
+
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
